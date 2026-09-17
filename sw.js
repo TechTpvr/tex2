8630bb981ffc7e1975cb82bfc58e0cd880b0cd71
TechTpvr <farrokhzadpvr@gmail.com>
2026-09-17 19:01:34 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='teexaa-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./logo-app.png','./teexaa-entry.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
